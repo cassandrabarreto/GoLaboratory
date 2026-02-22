6bc32bd470c03224f9295823f4fd6a382c07b126
Cassandra Barreto <casbarre@cisco.com>
2026-02-22 17:17:56 +0100

    Added Context Problen

---
+++ concurrencyProblems/Contexts/DataBaseQuery/databaseContext.go
@@ -0,0 +1,41 @@
+/*
+Timeout database query
+Simulate a slow database query that takes 3 seconds to respond.
+
+	But your context times out after 1 second, cancelling the query before it finishes.
+
+Constraints:
+
+Use context.WithTimeout this time instead of context.WithCancel
+Create a queryDB function that receives a context and simulates a slow query using time.Sleep(3 * time.Second) inside a goroutine
+Use select to either return the query result or cancel if ctx.Done() fires
+If cancelled, print "query timed out"
+If successful, print "query result: 42"
+Call defer cancel() in main right after creating the context
+*/
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func queryDB(ctx context.Context){
+	// Query Simulation
+
+	select{
+	case <- ctx.Done():
+		fmt.Println("Query Timed Out")
+	case <- time.After(3 * time.Second):
+		fmt.Println("Response received")
+	}
+}
+
+func main(){
+	ctx , cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	go queryDB(ctx)
+	time.Sleep(2 * time.Second)
+	
+}
